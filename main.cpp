f5aab962477af5d6af0cf94cad9dd582cdeb4425
Ziad-Elmasry <ziad.mohanad.0066@gmail.com>
2025-11-12 15:27:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,25 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
+    Enclosure* enclosure1 = new Enclosure(10);
+
+    Mammal* lion = new Mammal("Lion", 5, true, "Golden");
+    Bird* parrot = new Bird("Parrot", 2, false, 0.5f);
+    Reptile* snake = new Reptile("Snake", 3, true, true);
     
+    enclosure1->addAnimal(lion);
+    enclosure1->addAnimal(parrot);
+    enclosure1->addAnimal(snake);
+
+    Visitor* visitor = new Visitor("Sarah Ali", 3);
+    
+    enclosure1->displayAnimals();
+    visitor->displayInfo();
+    
+    delete enclosure1;
+    delete visitor;
     
     return 0;
 }
+    
+    
