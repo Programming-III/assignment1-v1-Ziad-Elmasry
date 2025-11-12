e88fd0fa118e420327a1b009ad68a34ee2635507
Ziad-Elmasry <ziad.mohanad.0066@gmail.com>
2025-11-12 15:22:10 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -4,7 +4,19 @@
 #include "Animal.h"
 
 //define reptile class here
+class Reptile : public Animal {
+private:
+    bool isVenomous;
 
+public:
+    Reptile(string n, int a, bool venomous, bool hungry = true) 
+        : Animal(n, a, hungry), isVenomous(venomous) {}
+    
+    void display() override {
+        Animal::display();
+        cout << "Type: Reptile, Venomous: " << (isVenomous ? "Yes" : "No") << endl;
+    }
+};
 
 
 
