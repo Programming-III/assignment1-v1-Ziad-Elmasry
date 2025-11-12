936fa7e268a333c9f790fa932eedad21c9b0b078
Ziad-Elmasry <ziad.mohanad.0066@gmail.com>
2025-11-12 15:17:07 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,36 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+//define animal class here class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
+
+public:
+    
+    Animal(string n, int a, bool hungry = true) : name(n), age(a), isHungry(hungry) {}
+    virtual ~Animal() {}
+    
+    virtual void display() {
+        cout << "Name: " << name << ", Age: " << age << " years, Hungry: " << (isHungry ? "Yes" : "No") << endl;
+    }
+    
+  
+    void feed() {
+        if (isHungry) {
+            cout << name << " has been fed and is no longer hungry." << endl;
+            isHungry = false;
+        } else {
+            cout << name << " is not hungry right now." << endl;
+        }
+    }
+    
+    
+    bool getHungerStatus() { return isHungry; }
+    void setHungerStatus(bool hungry) { isHungry = hungry; }
+};
+
 
 
 
