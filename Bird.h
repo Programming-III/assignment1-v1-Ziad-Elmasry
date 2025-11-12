ca55c2df5644de2a7d34b1c31ced3c8a2abfcde5
Ziad-Elmasry <ziad.mohanad.0066@gmail.com>
2025-11-12 15:18:29 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,7 +5,19 @@
 
 //define class bird here 
 
+class Bird : public Animal {
+private:
+    float wingSpan;
 
+public:
+    Bird(string n, int a, float span, bool hungry = true) 
+        : Animal(n, a, hungry), wingSpan(span) {}
+    
+    void display() override {
+        Animal::display();
+        cout << "Type: Bird, Wing Span: " << wingSpan << " meters" << endl;
+    }
+};
 
 
 
