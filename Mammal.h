66766f1f1ea07deee080602186d3896ef0e2eeef
Ziad-Elmasry <ziad.mohanad.0066@gmail.com>
2025-11-12 15:20:58 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,19 @@
 #include "Animal.h"
 
 //define mammal class here
+class Mammal : public Animal {
+private:
+    string furColor;
 
+public:
+    Mammal(string n, int a, string color, bool hungry = true) 
+        : Animal(n, a, hungry), furColor(color) {}
+    
+    void display() override {
+        Animal::display();
+        cout << "Type: Mammal, Fur Color: " << furColor << endl;
+    }
+};
 
 
 
